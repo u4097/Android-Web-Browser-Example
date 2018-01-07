f13a39bc8286d3e74148acd71aa773d817f0a574
Artem Fomchenkov <artem.fomchenkov@outlook.com>
2018-01-07 09:02:36 +0300

    Initial commit

---
+++ app/src/main/java/com/fomchenkovoutlook/artem/android_web_browser_example/WebBrowserActivity.java
@@ -0,0 +1,14 @@
+package com.fomchenkovoutlook.artem.android_web_browser_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebBrowserActivity
+        extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_browser);
+    }
+}
